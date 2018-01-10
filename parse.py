43475b08e05b4fbbc5423c0aac253d3b8299496f
limitriss <limitriss@gmail.com>
2018-01-11 01:59:18 +0900

    request_loop 함수로 중복제거

--- parse.py
+++ parse.py
@@ -58,16 +58,13 @@ class DartAPI:
                 return False
 
     @staticmethod
-    def get_recent_report(START_DATE, page_no=1):
+    def request_loop(query):
         """
         request 요청에 제한이 있어
         request 요청 사이에 기타작업(가공, DB) 가능하도록 제너레이터
-        :param START_DATE: 최근 3개월로 지정해야함
         :yield: 보고서리스트 (최대 100개) (제너레이터)
         """
-        query = DEFAULT_QUERY
-        query['start_dt'] = START_DATE
-        query['page_no'] = page_no
+
         url = DartAPI.url(query)
 
         while not DartAPI.can_request():
@@ -108,14 +105,23 @@ class DartAPI:
             yield data['list']
 
     @staticmethod
+    def get_recent_report(START_DATE, page_no=1):
+        """
+        :param START_DATE: 최근 3개월로 지정해야함
+        :return: DartAPI.request_loop(query)
+        """
+        query = DEFAULT_QUERY
+        query['start_dt'] = START_DATE
+        query['page_no'] = page_no
+        return DartAPI.request_loop(query)
+
+    @staticmethod
     def get_recent_report_by_type(START_DATE, page_no=1, 
         dsp_tp_list=None, 
         bsn_tp_list=None):
         """
-        request 요청에 제한이 있어
-        request 요청 사이에 기타작업(가공, DB) 가능하도록 제너레이터
         :param START_DATE: 최근 3개월로 지정해야함
-        :yield: 보고서리스트 (최대 100개) (제너레이터)
+        :return: DartAPI.request_loop(query)
         """
         query = DEFAULT_QUERY
         query['start_dt'] = START_DATE
@@ -126,51 +132,14 @@ class DartAPI:
         if bsn_tp_list:
             query['bsn_tp'] = bsn_tp_list
 
-        url = DartAPI.url(query)
-
-        while not DartAPI.can_request():
-            time.sleep(0.1)
-        r = requests.get(url)
-        DartAPI.recent_request = time.time()
-        data = json.loads(r.content.decode('utf8'))
-
-        err_code = data['err_code']
-        err_msg = data['err_msg']
-        page_no = data['page_no']
-        total_page = data['total_page']
-        if err_code != '000':
-            raise ValueError(err_msg, url)
-
-        print('page %s/%s' % (page_no, total_page))
-        yield data['list']
-
-        page_no += 1
-        while page_no <= total_page:
-            query['page_no'] = page_no
-            url = DartAPI.url(query)
-
-            while not DartAPI.can_request():
-                time.sleep(0.1)
-            r = requests.get(url)
-            DartAPI.recent_request = time.time()
-            data = json.loads(r.content.decode('utf8'))
-
-            err_code = data['err_code']
-            err_msg = data['err_msg']
-            page_no = data['page_no']
-            if err_code != '000':
-                raise ValueError(err_msg, url)
-
-            print('page %s/%s' % (page_no, total_page))
-            page_no += 1
-            yield data['list']
+        return DartAPI.request_loop(query)
 
 
 if __name__ == '__main__':
     for data in DartAPI.get_recent_report(20180101):
         print(data)
 
-    for data in DartAPI.get_recent_report_by_type(20180101,
-                                                    dsp_tp_list=['A'],
-                                                    bsn_tp_list=['B003']):
-        print(data)
+    # for data in DartAPI.get_recent_report_by_type(20180101,
+    #                                                 dsp_tp_list=['A'],
+    #                                                 bsn_tp_list=['B003']):
+    #     print(data)
